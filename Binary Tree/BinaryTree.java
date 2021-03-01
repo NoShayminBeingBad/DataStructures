af0bb5c9819abc25b8bd3ec20b6d63f4e36561c9
NoShayminBeingBad <37005204+NoShayminBeingBad@users.noreply.github.com>
2021-03-01 15:57:50 -0500

    Add files via upload

---
+++ Binary Tree/BinaryTree.java
@@ -0,0 +1,512 @@
+package comp2402a3;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.ArrayList;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+/**
+ * An implementation of binary trees
+ * @author morin
+ *
+ * @param <Node>
+ */
+public class BinaryTree<Node extends BinaryTree.BTNode<Node>> {
+
+	public static class BTNode<Node extends BTNode<Node>> {
+		public Node left;
+		public Node right;
+		public Node parent;
+	}
+
+	/**
+	 * An extension of BTNode that you can actually instantiate.
+	 */
+	protected static class EndNode extends BTNode<EndNode> {
+			public EndNode() {
+				this.parent = this.left = this.right = null;
+			}
+	}
+
+	/**
+	 * Used to make a mini-factory
+	 */
+	protected Node sampleNode;
+
+	/**
+	 * The root of this tree
+	 */
+	protected Node r;
+
+	/**
+	 * This tree's "null" node
+	 */
+	protected Node nil;
+
+	/**
+	 * Create a new instance of this class
+	 * @param sampleNode - a sample of a node that can be used
+	 * to create a new node in newNode()
+	 * @param nil - a node that will be used in place of null
+	 */
+	public BinaryTree(Node sampleNode, Node nil) {
+		this.sampleNode = sampleNode;
+		this.nil = nil;
+		r = nil;
+	}
+
+	/**
+	 * Create a new instance of this class
+	 * @param sampleNode - a sample of a node that can be used
+	 * to create a new node in newNode()
+	 */
+	public BinaryTree(Node sampleNode) {
+		this.sampleNode = sampleNode;
+	}
+
+	/**
+	 * Allocate a new node for use in this tree
+	 * @return
+	 */
+	@SuppressWarnings({"unchecked"})
+	protected Node newNode() {
+		try {
+			Node u = (Node)sampleNode.getClass().getDeclaredConstructor().newInstance();
+			u.parent = u.left = u.right = nil;
+			return u;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	/**
+	 * Compute the depth (distance to the root) of u
+	 * @param u
+	 * @return the distanct between u and the root, r
+	 */
+	public int depth(Node u) {
+		int d = 0;
+		while (u != r) {
+			u = u.parent;
+			d++;
+		}
+		return d;
+	}
+
+	/**
+	 * Compute the size (number of nodes) of this tree
+	 * @warning uses recursion so could cause a stack overflow
+	 * @return the number of nodes in this tree
+	 */
+	public int size() {
+		return size(r);
+	}
+
+	/**
+	 * @return the size of the subtree rooted at u
+	 */
+	protected int size(Node u) {
+		if (u == nil) return 0;
+		return 1 + size(u.left) + size(u.right);
+	}
+
+	/**
+	 * Compute the number of nodes in this tree without recursion
+	 * @return
+	 */
+	public int size2() {
+		Node u = r, prev = nil, next;
+		int n = 0;
+		while (u != nil) {
+			if (prev == u.parent) {
+				n++;
+				if (u.left != nil) next = u.left;
+				else if (u.right != nil) next = u.right;
+				else next = u.parent;
+			} else if (prev == u.left) {
+				if (u.right != nil) next = u.right;
+				else next = u.parent;
+			} else {
+				next = u.parent;
+			}
+			prev = u;
+			u = next;
+		}
+		return n;
+	}
+
+	/**
+	 * Compute the maximum depth of any node in this tree
+	 * @return the maximum depth of any node in this tree
+	 */
+	public int height() {
+		return height(r);
+	}
+
+	/**
+	 * @return the height of the subtree rooted at u
+	 */
+	protected int height(Node u) {
+		if (u == nil) return -1;
+		return 1 + Math.max(height(u.left), height(u.right));
+	}
+
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		toStringHelper(sb, r);
+		return sb.toString();
+	}
+
+	protected void toStringHelper(StringBuilder sb, Node u) {
+			if (u == null) {
+				return;
+			}
+			sb.append('(');
+			toStringHelper(sb, u.left);
+			toStringHelper(sb, u.right);
+			sb.append(')');
+	}
+
+
+	/**
+	 * @ return an n-node BinaryTree that has the shape of a random
+	 * binary search tree.
+	 */
+	public static BinaryTree<EndNode> randomBST(int n) {
+		Random rand = new Random();
+		EndNode sample = new EndNode();
+		BinaryTree<EndNode> t = new BinaryTree<EndNode>(sample);
+		t.r = randomBSTHelper(n, rand);
+		return t;
+	}
+
+	protected static EndNode randomBSTHelper(int n, Random rand) {
+		if (n == 0) {
+			return null;
+		}
+		EndNode r = new EndNode();
+		int ml = rand.nextInt(n);
+		int mr = n - ml - 1;
+		if (ml > 0) {
+			r.left = randomBSTHelper(ml, rand);
+			r.left.parent = r;
+		}
+		if (mr > 0) {
+			r.right = randomBSTHelper(mr, rand);
+			r.right.parent = r;
+		}
+		return r;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return r == nil;
+	}
+
+	/**
+	 * Make this tree into the empty tree
+	 */
+	public void clear() {
+		r = nil;
+	}
+
+	/**
+	 * Demonstration of a recursive traversal
+	 * @param u
+	 */
+	public void traverse(Node u) {
+		if (u == nil) return;
+		traverse(u.left);
+		traverse(u.right);
+	}
+
+	/**
+	 * Demonstration of a non-recursive traversal
+	 */
+	public void traverse2() {
+		Node u = r, prev = nil, next;
+		while (u != nil) {
+			if (prev == u.parent) {
+				if (u.left != nil) {
+					next = u.left;
+				} else if (u.right != nil) {
+					next = u.right;
+				}	else {
+					next = u.parent;
+				}
+			} else if (prev == u.left) {
+				if (u.right != nil) {
+					next = u.right;
+				} else {
+					next = u.parent;
+				}
+			} else {
+				next = u.parent;
+			}
+			prev = u;
+			u = next;
+		}
+	}
+
+	/**
+	 * Demonstration of a breadth-first traversal
+	 */
+	public void bfTraverse() {
+		Queue<Node> q = new LinkedList<Node>();
+		if (r != nil) q.add(r);
+		while (!q.isEmpty()) {
+			Node u = q.remove();
+			if (u.left != nil) q.add(u.left);
+			if (u.right != nil) q.add(u.right);
+		}
+	}
+
+	/**
+	 * Find the first node in an in-order traversal
+	 * @return the first node reported in an in-order traversal
+	 */
+	public Node firstNode() {
+		Node w = r;
+		if (w == nil) return nil;
+		while (w.left != nil)
+			w = w.left;
+		return w;
+	}
+
+	/**
+	 * Find the node that follows w in an in-order traversal
+	 * @param w
+	 * @return the node that follows w in an in-order traversal
+	 */
+	public Node nextNode(Node w) {
+		if (w.right != nil) {
+			w = w.right;
+			while (w.left != nil)
+				w = w.left;
+		} else {
+			while (w.parent != nil && w.parent.left != w)
+				w = w.parent;
+			w = w.parent;
+		}
+		return w;
+	}
+
+	public int totalDepth() {
+		Node u = r, prev = nil, next;
+		int d = 0;
+		int total = 0;
+		while (u != nil){
+			if (prev == u.parent){
+				if (u.left != nil){
+					next = u.left;
+					d++;
+				}else if (u.right != nil){
+					next = u.right;
+					d++;
+				}else {
+					next = u.parent;
+					total += d;
+					d--;
+				}
+			}else if (prev == u.left){
+				if (u.right != nil){
+					next = u.right;
+					d++;
+				}else {
+					next = u.parent;
+					total += d;
+					d--;
+				}
+			}else {
+				next = u.parent;
+				total += d;
+				d--;
+			}
+			prev = u;
+			u = next;
+		}
+		return total;
+	}
+
+	public int totalLeafDepth() {
+		Node u = r, prev = nil, next;
+		int d = 0;
+		int total = 0;
+		while (u != nil){
+			if (prev == u.parent){
+				if (u.left != nil){
+					next = u.left;
+					d++;
+				}else if (u.right != nil){
+					next = u.right;
+					d++;
+				}else {
+					next = u.parent;
+					total += d;
+					d--;
+				}
+			}else if (prev == u.left){
+				if (u.right != nil){
+					next = u.right;
+					d++;
+				}else {
+					next = u.parent;
+					d--;
+				}
+			}else if (prev == u.right){
+				next = u.parent;
+				d--;
+			}else {
+				next = u.parent;
+				total += d;
+				d--;
+			}
+			prev = u;
+			u = next;
+		}
+		return total;
+	}
+
+	public String bracketSequence() {
+		StringBuilder sb = new StringBuilder();
+		Node u = r, prev = nil, next;
+		if (r == nil) return ".";
+		sb.append("(");
+		while (u != nil){
+			if (prev == u.parent){
+				if (u.left != nil){
+					next = u.left;
+					sb.append("(");
+				}else if (u.right != nil){
+					next = u.right;
+					sb.append(".(");
+				}else {
+					next = u.parent;
+					sb.append("..)");
+				}
+			}else if (prev == u.left){
+				if (u.right != nil){
+					next = u.right;
+					sb.append("(");
+				}else {
+					next = u.parent;
+					sb.append(".)");
+				}
+			}else if (prev == u.right){
+				next = u.parent;
+				sb.append(")");
+			}else {
+				next = u.parent;
+				sb.append(")");
+			}
+			prev = u;
+			u = next;
+		}
+		return sb.toString();
+	}
+
+	public void prettyPrint(PrintWriter w) {
+		if (r == nil)return;
+		ArrayList<StringBuilder> arr = new ArrayList<StringBuilder>();
+		Node u = r, prev = nil, next;
+		int d = 0;
+		int len = 0;
+		arr.add(new StringBuilder());
+		arr.get(d).append("*");
+		while (u != nil){
+			//System.out.println("arr: " + arr.get(0).length() + " len: " + d);
+			if (prev == u.parent){
+				if (u.left != nil){
+					next = u.left;
+					d++;
+					if (d >= arr.size()){
+						arr.add(new StringBuilder());
+					}
+					//System.out.println("arr: " + arr.get(d).length() + " len: " + len);
+					while (arr.get(d).length() < len){
+						arr.get(d).append(" ");
+					}
+					arr.get(d).append("*");
+				}else if (u.right != nil){
+					next = u.right;
+					len += 2;
+					//System.out.println("arr: " + arr.get(d).length() + " len: " + len);
+					while (arr.get(d).length() < len){
+						arr.get(d).append("-");
+					}
+					arr.get(d).append("*");
+				}else {
+					next = u.parent;
+					if (d > 0 && u.parent.left == u)
+						d--;
+				}
+			}else if (prev == u.left){
+				if (u.right != nil){
+					next = u.right;
+					len += 2;
+					//System.out.println("arr: " + arr.get(d).length() + " len: " + d);
+					while (arr.get(d).length() < len){
+						arr.get(d).append("-");
+					}
+					arr.get(d).append("*");
+				}else {
+					next = u.parent;
+					if (d > 0 && u.parent.left == u)
+						d--;
+				}
+			}else if (prev == u.right){
+				next = u.parent;
+				if (d > 0 && u.parent.left == u)
+					d--;
+			}else {
+				next = u.parent;
+				if (d > 0 && u.parent.left == u)
+					d--;
+			}
+			prev = u;
+			u = next;
+		}
+		for (int i = 0; i < arr.size() - 1; i++){
+			w.println(arr.get(i));
+			for (int j = 0; j < arr.get(i).length(); j++){
+				if (arr.get(i+1).length() >= j &&arr.get(i).charAt(j) == '*' && arr.get(i+1).charAt(j) == '*'){
+					w.print("|");
+				}else {
+					w.print(" ");
+				}
+			}
+			w.println();
+		}
+		w.println(arr.get(arr.size() - 1));
+	}
+
+	public static void main(String[] args) {
+		try {
+			PrintWriter w;
+			if (args.length == 0) {
+				w = new PrintWriter(System.out);
+			} else if (args.length == 1) {
+				w = new PrintWriter(System.out);				
+			} else {
+				w = new PrintWriter(new FileWriter(args[1]));
+			}
+			BinaryTree<EndNode> a =randomBST(100);
+			a.prettyPrint(w);
+			//System.out.println(a.bracketSequence());
+			w.flush();
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(-1);
+		}
+	}
+
+}
---
+++ Skip List/FastSkiplistRankedSSet.java
@@ -0,0 +1,348 @@
+package comp2402a3;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * An implementation of skiplists for searching
+ *
+ * @author morin
+ *
+ * @param <T>
+ */
+public class FastSkiplistRankedSSet<T> implements RankedSSet<T> {
+	protected Comparator<T> c;
+
+	@SuppressWarnings("unchecked")
+	protected static class Node<T> {
+		T x;
+		int[] length;
+		Node<T>[] next;
+		public Node(T ix, int h) {
+			x = ix;
+			length = new int[h + 1];
+			next = (Node<T>[])Array.newInstance(Node.class, h+1);
+		}
+		public int height() {
+			return next.length - 1;
+		}
+	}
+
+	/**
+	 * This node<T> sits on the left side of the skiplist
+	 */
+	protected Node<T> sentinel;
+
+	/**
+	 * The maximum height of any element
+	 */
+	int h;
+
+	/**
+	 * The number of elements stored in the skiplist
+	 */
+	int n;
+
+	/**
+	 * A source of random numbers
+	 */
+	Random rand;
+
+	/**
+	 * Used by add(x) method
+	 */
+	protected Node<T>[] stack;
+
+	@SuppressWarnings("unchecked")
+	public FastSkiplistRankedSSet(Comparator<T> c) {
+		this.c = c;
+		n = 0;
+		sentinel = new Node<T>(null, 32);
+		stack = (Node<T>[])Array.newInstance(Node.class, sentinel.next.length);
+		h = 0;
+		rand = new Random();
+	}
+
+	public FastSkiplistRankedSSet() {
+		this(new DefaultComparator<T>());
+	}
+
+	/**
+	 * Find the node<T> u that precedes the value x in the skiplist.
+	 *
+	 * @param x - the value to search for
+	 * @return a node<T> u that maximizes u.x subject to
+	 * the constraint that u.x < x --- or sentinel if u.x >= x for
+	 * all node<T>s x
+	 */
+	protected Node<T> findPredNode(T x) {
+		Node<T> u = sentinel;
+		int r = h;
+		while (r >= 0) {
+			while (u.next[r] != null && c.compare(u.next[r].x,x) < 0)
+				u = u.next[r];   // go right in list r
+			r--;               // go down into list r-1
+		}
+		return u;
+	}
+
+	public T find(T x) {
+		Node<T> u = findPredNode(x);
+		return u.next[0] == null ? null : u.next[0].x;
+	}
+
+	public T findGE(T x) {
+		if (x == null) {   // return first node<T>
+			return sentinel.next[0] == null ? null : sentinel.next[0].x;
+		}
+		return find(x);
+	}
+
+	public T findLT(T x) {
+		if (x == null) {  // return last node<T>
+			Node<T> u = sentinel;
+			int r = h;
+			while (r >= 0) {
+				while (u.next[r] != null)
+					u = u.next[r];
+				r--;
+			}
+			return u.x;
+		}
+		return findPredNode(x).x;
+	}
+
+	public T get(int i) {
+		Node<T> u = sentinel;
+		int j = -1;
+		int r = h;
+		while (r >= 0){
+			while (u.next[r] != null && j + u.length[r] <= i){
+				j += u.length[r];
+				u = u.next[r];
+			}
+			r--;
+		}
+		return u.x;
+	}
+
+	public int rank(T x) {
+		Node<T> u = sentinel;
+		int j = 0;
+		int r = h;
+		while (r >= 0){
+			while (u.next[r] != null && c.compare(u.next[r].x, x) < 0){
+				j += u.length[r];
+				u = u.next[r];
+			}
+			r--;
+		}
+		return j;
+	}
+
+	public boolean remove(T x) {
+		boolean removed = false;
+		Node<T> u = sentinel;
+		int r = h;
+		int comp = 0;
+		while (r >= 0) {
+			while (u.next[r] != null
+			       && (comp = c.compare(u.next[r].x, x)) < 0) {
+				u = u.next[r];
+			}
+			stack[r] = u;
+			if (u.next[r] != null && comp == 0) {
+				removed = true;
+				u.length[r] += u.next[r].length[r];
+				u.next[r] = u.next[r].next[r];
+				if (u == sentinel && u.next[r] == null)
+					h--;  // height has gone down
+			}
+			r--;
+		}
+		
+		if (removed){
+			for (int i = 0; i < h + 1; i++){
+				if (stack[i].length[i] > 0)
+					stack[i].length[i]--;
+			}
+			n--;
+		}
+		return removed;
+	}
+
+
+	/**
+	 * Simulate repeatedly tossing a coin until it comes up tails.
+	 * Note, this code will never generate a height greater than 32
+	 * @return the number of coin tosses - 1
+	 */
+	protected int pickHeight() {
+		int z = rand.nextInt();
+		int k = 0;
+		int m = 1;
+		while ((z & m) != 0) {
+			k++;
+			m <<= 1;
+		}
+		return k;
+	}
+
+	public void clear() {
+		n = 0;
+		h = 0;
+		Arrays.fill(sentinel.next, null);
+	}
+
+	public int size() {
+		return n;
+	}
+
+	public Comparator<T> comparator() {
+		return c;
+	}
+
+	/**
+	 * Create a new iterator in which the next value in the iteration is u.next.x
+	 * TODO: Constant time removal requires the use of a skiplist finger (a stack)
+	 * @param u
+	 * @return
+	 */
+	protected Iterator<T> iterator(Node<T> u) {
+		class SkiplistIterator implements Iterator<T> {
+			Node<T> u, prev;
+			public SkiplistIterator(Node<T> u) {
+				this.u = u;
+				prev = null;
+			}
+			public boolean hasNext() {
+				return u.next[0] != null;
+			}
+			public T next() {
+				prev = u;
+				u = u.next[0];
+				return u.x;
+			}
+			public void remove() {
+				// Not constant time
+				FastSkiplistRankedSSet.this.remove(prev.x);
+			}
+		}
+		return new SkiplistIterator(u);
+	}
+
+	public Iterator<T> iterator() {
+		return iterator(sentinel);
+	}
+
+	public Iterator<T> iterator(T x) {
+		return iterator(findPredNode(x));
+	}
+
+	public boolean add(T x) {
+		Node<T> u = sentinel;
+		int r = h;
+		int comp = 0;
+		int[] a = new int[stack.length];
+		int j = 0;
+		while (r >= 0){
+			while (u.next[r] != null && (comp = c.compare(u.next[r].x,x)) < 0){
+				j += u.length[r];
+				u = u.next[r];
+			}
+			a[r] = j;
+			if (u.next[r] != null && comp == 0) return false;
+			stack[r--] = u;
+		}
+		Node<T> w = new Node<T>(x, pickHeight());
+		while (h < w.height())
+			stack[++h] = sentinel;
+		int i = 0;
+		while(i < h + 1){
+			if (i < w.next.length) {
+				w.next[i] = stack[i].next[i];
+				stack[i].next[i] = w;
+				w.length[i] = stack[i].length[i] - (j - a[i]);
+				if (w.length[i] < 0) w.length[i] = 0;
+				stack[i].length[i] = j - a[i] + 1;
+			}else{
+				if(stack[i].next[i] != null)
+					stack[i].length[i]++;
+			}
+			i++;
+		}
+		n++;
+		return true;
+	}
+	
+	public void display(){
+		Node<T> it = this.sentinel;
+		for (int i = h; i >= 0; i--){
+			for (int j = 0; j < n + 1; j++){
+				if (it.height() >= i){
+					System.out.print("*");
+				}else {
+					System.out.print("-");
+				}
+				it = it.next[0];
+			}
+			System.out.println("");
+			it = this.sentinel;
+		}
+		System.out.println("");
+		it = this.sentinel;
+		for (int i = h; i >= 0; i--){
+			for (int j = 0; j < n + 1; j++){
+				if (it.height() >= i){
+					System.out.print(it.length[i]);
+				}else {
+					System.out.print("_");
+				}
+				it = it.next[0];
+			}
+			System.out.println("");
+			it = this.sentinel;
+		}
+		System.out.println("end");
+	}
+
+	public static void main(String[] args) {
+		int n = 20;
+
+		Random rand = new java.util.Random();
+		FastSkiplistRankedSSet<Integer> rss = new FastSkiplistRankedSSet<>();
+		for (int i = 0; i < n; i++) {
+			int a = rand.nextInt(3*n);
+			rss.add(a);
+			//rss.print();
+		}
+		rss.add(12);
+		System.out.print("Contents: ");
+		for (Integer x : rss) {
+			System.out.print(x + ",");
+		}
+		System.out.println();
+		rss.display();
+		System.out.println(rss.remove(12));
+		//rss.remove(13);
+		//rss.remove(100);
+		rss.display();
+		System.out.println("size()=" + rss.size());
+		System.out.print("Contents: ");
+		for (Integer x : rss) {
+			System.out.print(x + ",");
+		}
+		for (int i = 0; i < rss.size(); i++) {
+			Integer x = rss.get(i);
+			System.out.println("get(" + i + ")=" + x);
+		}
+
+		for (Integer x = 0; x < 3*n+1; x++) {
+			int i = rss.rank(x);
+			System.out.println("rank(" + x + ")=" + i);
+		}
+	}
+}
